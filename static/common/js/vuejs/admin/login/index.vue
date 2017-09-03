4454a0df3cc00717d231c7296b25f8f8d4dedbc1
赵钟品 <iteny@msn.com>
2017-09-03 22:29:32 +0800

    no message

--- static/common/js/vuejs/admin/login/index.vue
+++ static/common/js/vuejs/admin/login/index.vue
@@ -62,7 +62,25 @@ body{
 export default {
     methods: {
         submitForm(formName) {
-            alert('asdfsadf');
+            //自定义验证规则
+            form.verify({
+                title: function(value){
+                  if(value.length < 5){
+                    return '标题至少得5个字符啊';
+                  }
+                }
+                ,pass: [/(.+){6,12}$/, '密码必须6到12位']
+                ,content: function(value){
+                  layedit.sync(editIndex);
+                }
+            });
+            //监听提交
+            form.on('submit(demo1)', function(data){
+                layer.alert(JSON.stringify(data.field), {
+                  title: '最终的提交信息'
+                })
+                return false;
+            });
         }
 
     }
