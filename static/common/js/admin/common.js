2988a1d6f4925c86fe256d851cb24bf9e8364007
赵钟品 <iteny@msn.com>
2017-09-08 00:24:55 +0800

    no message

--- static/common/js/admin/common.js
+++ static/common/js/admin/common.js
@@ -10,17 +10,17 @@ var hm = {
      */
     submit:function(submitName,submitUrl){
         //自定义验证规则
-        // form.verify({
-        //     title: function(value){
-        //       if(value.length < 5){
-        //         return '标题至少得5个字符啊';
-        //       }
-        //     }
-        //     ,pass: [/(.+){6,12}$/, '密码必须6到12位']
-        //     ,content: function(value){
-        //       layedit.sync(editIndex);
-        //     }
-        // });
+        form.verify({
+            title: function(value){
+              if(value.length < 5){
+                return '标题至少得5个字符啊';
+              }
+            }
+            ,pass: [/(.+){6,12}$/, '密码必须6到12位']
+            ,content: function(value){
+              layedit.sync(editIndex);
+            }
+        });
         //监听提交
         form.on('submit('+submitName+')', function(data){
             // layer.alert(JSON.stringify(data.field), {
